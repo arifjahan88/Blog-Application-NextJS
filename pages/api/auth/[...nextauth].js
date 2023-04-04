e5d675d06cddd497fec9911404f7be6e45e32319
Arif Jahan <56018713+arifjahan88@users.noreply.github.com>
2023-04-04 19:27:57 +0600

    final done

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import GithubProvider from "next-auth/providers/github";
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: "5ba1a4195f5bc6220384",
+      clientSecret: "3bce2c0472dc244555b2c8110cef97145fee5b28",
     }),
   ],
 };
